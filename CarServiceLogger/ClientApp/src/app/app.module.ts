22232fddec984b800bbc6eb9e80bdc6295ab5bf7
alexfeds <alexfeds@gmail.com>
2021-01-01 23:31:31 +0000

    log in configured, using azure ad

--- CarServiceLogger/ClientApp/src/app/app.module.ts
+++ CarServiceLogger/ClientApp/src/app/app.module.ts
@@ -17,15 +17,15 @@ import { AuthInterceptor } from './auth.interceptor';
 export function configureAuth(oidcConfigService: OidcConfigService) {
   return () =>
     oidcConfigService.withConfig({
-      stsServer: 'https://login.microsoftonline.com/7ff95b15-dc21-4ba6-bc92-824856578fc1/v2.0',
-      authWellknownEndpoint: 'https://login.microsoftonline.com/7ff95b15-dc21-4ba6-bc92-824856578fc1/v2.0',
+      stsServer: 'https://login.microsoftonline.com/fb007b00-988e-4e16-954c-f02932502996/v2.0',
+      authWellknownEndpoint: 'https://login.microsoftonline.com/fb007b00-988e-4e16-954c-f02932502996/v2.0',
       redirectUrl: window.location.origin,
       clientId: '15537a2c-313f-4abd-81ab-2866b27ca3c6',
       scope: 'openid profile email api://15537a2c-313f-4abd-81ab-2866b27ca3c6/access_as_user',
       responseType: 'code',
       silentRenew: true,
       maxIdTokenIatOffsetAllowedInSeconds: 600,
-      issValidationOff: false, // this needs to be true if using a common endpoint in Azure
+      issValidationOff: true, // this needs to be true if using a common endpoint in Azure
       autoUserinfo: false,
       silentRenewUrl: window.location.origin + '/silent-renew.html',
       logLevel: LogLevel.Debug
