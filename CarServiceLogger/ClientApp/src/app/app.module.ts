c1ac024bc5c519b1d8e25646ba030540fe78c3a3
alexfeds <alexfeds@gmail.com>
2021-01-01 23:33:10 +0000

    Add authentication (#1)
    * configure auth
    
    * angular 8 update
    
    * angular 9 update
    
    * locolaize update
    
    * log in configured, using azure ad

--- CarServiceLogger/ClientApp/src/app/app.module.ts
+++ CarServiceLogger/ClientApp/src/app/app.module.ts
@@ -1,14 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, APP_INITIALIZER } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-
+import { AuthModule, OidcConfigService, LogLevel } from 'angular-auth-oidc-client';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
+import { UnauthorizedComponent } from './unauthorized/unauthorized.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { AuthInterceptor } from './auth.interceptor';
+
+
+
+export function configureAuth(oidcConfigService: OidcConfigService) {
+  return () =>
+    oidcConfigService.withConfig({
+      stsServer: 'https://login.microsoftonline.com/fb007b00-988e-4e16-954c-f02932502996/v2.0',
+      authWellknownEndpoint: 'https://login.microsoftonline.com/fb007b00-988e-4e16-954c-f02932502996/v2.0',
+      redirectUrl: window.location.origin,
+      clientId: '15537a2c-313f-4abd-81ab-2866b27ca3c6',
+      scope: 'openid profile email api://15537a2c-313f-4abd-81ab-2866b27ca3c6/access_as_user',
+      responseType: 'code',
+      silentRenew: true,
+      maxIdTokenIatOffsetAllowedInSeconds: 600,
+      issValidationOff: true, // this needs to be true if using a common endpoint in Azure
+      autoUserinfo: false,
+      silentRenewUrl: window.location.origin + '/silent-renew.html',
+      logLevel: LogLevel.Debug
+    });
+}
+
+
 
 @NgModule({
   declarations: [
@@ -23,12 +47,27 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
-    ])
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', component: HomeComponent },
+      { path: 'unauthorized', component: UnauthorizedComponent },
+    ]),
+    HttpClientModule,
+    AuthModule.forRoot(),
   ],
-  providers: [],
+  providers: [
+    OidcConfigService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: configureAuth,
+      deps: [OidcConfigService],
+      multi: true,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
